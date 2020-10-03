4917def199e445bb20f39bbda688e3267479d31e
David Purdum <purdum41@gmail.com>
2020-10-03 07:12:48 -0400

    Final C changes

--- c/test.c
+++ c/test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
 
 // const* cant change data
 // *const cant change pointer
@@ -10,23 +11,42 @@ void mutate(size_t len, int a[len]) {
     int* b = {0};
 }
 
+void birthday(int* age) {
+    *age += 1;
+}
+
 int main() {
     int a[5] = {0};
 
     mutate(5, a);
 
-    for (int i = 0; i < 5; i++) {
-        printf("%i ", a[i]);
-    }
-    printf("\nEnter a num: ");
+    //for (int i = 0; i < 5; i++) {
+    //    printf("%i ", a[i]);
+    //}
+    //printf("\nEnter a num: ");
 
     int b = 0;
+    /*
     int result = scanf("%i", &b);
     if (!!result) {
         printf("double your num: %i\n", b * 2);
     } else {
         printf("thats not an integer!\n");
     }
+    */
+    char letter = '\0';
+    int success = read(STDIN_FILENO, &letter, 1);
+    if (!success) return EXIT_FAILURE;
+    printf("...You typed %c\n", letter);
+
+    /*
+    int age = 20;
+    birthday(&age);
+    printf("%i", age);
+
+    char* names[3] = {"asdf", "ab", "c"};
+    printf("%s", names[0]);
+    */
 
     return EXIT_SUCCESS;
 }
